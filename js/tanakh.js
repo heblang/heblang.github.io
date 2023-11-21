1155a23cc6cafd1a528b1152d3956ad47fee7705
aramlang <100400031+aramlang@users.noreply.github.com>
2023-11-21 04:42:08 -0600

    Less decimals

--- js/tanakh.js
+++ js/tanakh.js
@@ -12,53 +12,53 @@ tanakh.books = [
       [ // Verse 1
         {},
         {
-          a: "בְּרֵאשִׁ֖ית", // word
-          i: "In the beginning", // interlinear
-          p: "be·re·Shit", // phonetic
-          l: "bə·rê·šîṯ", // latin
-          t: 0.5 // cue
+          a: "בְּרֵאשִׁ֖ית",
+          i: "In the beginning",
+          p: "be·re·Shit",
+          l: "bə·rê·šîṯ",
+          t: 0.5,
         },
         {
           a: "בָּרָ֣א",
           i: "created",
           p: "ba·Ra",
           l: "bā·rā",
-          t: 1.5383408919123205
+          t: 1.53834,
         },
         {
           a: "אֱלֹהִ֑ים",
           i: "God",
           p: "E·lo·Him;",
           l: "’ĕ·lō·hîm;",
-          t: 2.2301177967274546
+          t: 2.23012,
         },
         {
           a: "אֵ֥ת",
           i: "-",
           p: "'et",
           l: "’êṯ",
-          t: 2.9949768203577722
+          t: 2.99498,
         },
         {
           a: "הַשָּׁמַ֖יִם",
           i: "the heavens",
           p: "hash·sha·Ma·yim",
           l: "haš·šā·ma·yim",
-          t: 3.413245707806932
+          t: 3.41325,
         },
         {
           a: "וְאֵ֥ת",
           i: "and",
           p: "ve·'Et",
           l: "wə·’êṯ",
-          t: 4.315797794583146
+          t: 4.3158,
         },
         {
           a: "הָאָֽרֶץ׃",
           i: "the earth",
           p: "ha·'A·retz.",
           l: "hā·’ā·reṣ.",
-          t: 4.75609808462392
+          t: 4.7561,
         }
       ]
     ]
